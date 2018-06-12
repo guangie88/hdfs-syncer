9bcf7fa7bd7e841978ea53ab490eb8d86425ba5e
Chen Weiguang <chen.weiguang@gmail.com>
2018-06-12 10:28:47 +0800

    Got working solution

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -3,11 +3,21 @@ import java.net.URI;
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.fs.FileSystem;
 import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.security.UserGroupInformation 
 
 object Main extends App {
   println("Hello, World!")
 
   val conf = new Configuration()
-  val fs = FileSystem.get(new URI("hdfs://localhost:8020"), conf)
+
+  conf.set("fs.defaultFS", "hdfs://localhost:8020")
+  conf.set("hadoop.security.authentication", "kerberos")
+  conf.set("hadoop.rpc.protection", "integrity");
+  conf.set("dfs.namenode.kerberos.principal.pattern", "hdfs/localhost@esciencecenter.nl");
+
+  UserGroupInformation.setConfiguration(conf);
+  UserGroupInformation.loginUserFromKeytab("hdfs/localhost@esciencecenter.nl", "./config/krb5.keytab");
+
+  val fs = FileSystem.get(conf)
   val output = fs.create(new Path("/test.txt"))
 }
